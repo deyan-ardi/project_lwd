df4ebc9b305f334c8349edff907c7e2241b1fe0c
Learn With Deyan <riyan.clsg11@gmail.com>
2020-08-20 21:36:09 +0800

    memperbaiki entah dimana

--- application/views/auth/reset_password.php
+++ application/views/auth/reset_password.php
@@ -38,6 +38,35 @@ $this->load->view('user/templates/header', $data);
                         <button class="btn-primary btn form-control" id="loginbutton" type="submit">Ganti
                             Kata Sandi</button>
                     </div>
+                    
+                    
+                    <h1><?php echo lang('reset_password_heading');?></h1>
+
+<div id="infoMessage"><?php echo $message;?></div>
+
+<?php echo form_open('auth/reset_password/' . $code);?>
+
+	<p>
+		<label for="new_password"><?php echo sprintf(lang('reset_password_new_password_label'), $min_password_length);?></label> <br />
+		<?php echo form_input($new_password);?>
+	</p>
+
+	<p>
+		<?php echo lang('reset_password_new_password_confirm_label', 'new_password_confirm');?> <br />
+		<?php echo form_input($new_password_confirm);?>
+	</p>
+
+	<?php echo form_input($user_id);?>
+	<?php echo form_hidden($csrf); ?>
+
+	<p><?php echo form_submit('submit', lang('reset_password_submit_btn'));?></p>
+
+<?php echo form_close();?>
+                    
+                    
+                    
+                    
+                    
 
                     <?php echo form_close(); ?>
                 </div>
